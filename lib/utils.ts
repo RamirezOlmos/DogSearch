00cb470c139b1a90f1e11738ba2b4d262e86cdac
RamirezOlmos <nyy_715074@yahoo.com>
2023-11-14 08:40:43 -0600

    created two new functions to get more than one breed in the api request

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { Dogs } from "@/types";
+import axios from "axios";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -16,4 +18,29 @@ export const updateSearchParams = (type: string, value: string) => {
   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
   return newPathname;
+};
+
+export const chunkArray = (array: string[], chunkSize: number) => {
+  const chunkedArray = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    chunkedArray.push(chunk);
+  }
+  return chunkedArray;
+};
+
+export const arrayOfDogs = async (array: string[][]) => {
+  const arrayDogs = [];
+  for (let i = 0; i < array.length; i += 1) {
+    const dogs = await axios.post(
+      `https://frontend-take-home-service.fetch.com/dogs`,
+      array[i],
+      {
+        withCredentials: true,
+      }
+    );
+    arrayDogs.push(dogs.data);
+  }
+
+  return arrayDogs;
 };
